3b924155fb85af386b6faa10d2ad683c22a3a71d
Matt Powell <fauxparse@gmail.com>
2015-01-07 11:40:14 +1300

    make login look bearable (still plenty to do)

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,5 +13,6 @@ Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( login.css login.js )
 
 # rubocop:enable Metrics/LineLength
